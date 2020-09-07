e2cea9818bb2dbec4f1f4b517a98abce236e6111
Yassine <yassinebyyn@outlook.com>
2020-09-07 10:50:56 +0200

    yassin

---
+++ nom_prenom_test_javascript/public/js/main.js
@@ -0,0 +1,79 @@
+// 1
+
+// let maBoolean = "Boolean"
+
+//let maBoolean = [true]
+//MaBoolean = Boolean
+
+
+// // 2
+
+// let monArray = []
+//console.log(MonArray);
+//MonArray.push('Yass');
+
+// // 3
+
+// let monObjet = {
+//     nome: "bouy"
+// }
+
+// // 4
+
+// let monString = 'yass'
+
+// // 5
+
+// let = nombre(24)
+
+// // 6
+
+// let monObjet = {
+
+//     nome: "Bouy",
+//     prenom: "yass",
+// }
+
+// nomeComplet = function() {
+// return nome + " " + prenom;
+// };
+//      console.log(nomeComplet);
+
+// //7
+
+// function N (nome,prenom);
+
+// 8
+
+// let  monInt = "monInt"
+// if (monInt<10) {
+//     console.log("oui");
+// }
+// else{
+//     console.log("non");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
